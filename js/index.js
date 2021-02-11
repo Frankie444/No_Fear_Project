9dd4e62a3c3dccba6318b144382e372b46e8ea4f
Frankie <75957770+Frankie444@users.noreply.github.com>
2021-02-11 21:58:55 +1100

    add TaskManager class and methods to .js

--- js/index.js
+++ js/index.js
@@ -1,58 +1,38 @@
-$('#validateForm').bootstrapValidator({
-    feedbackIcons: {
-        valid: 'glyphicon glyphicon-ok',
-        invalid: 'glyphicon glyphicon-remove',
-        validating: 'glyphicon glyphicon-refresh'
-    },
+//below creates an object instance using the TaskManager class
+const anotherTask = new TaskManager(0);
+console.log(anotherTask);
 
-    fields: {
-        taskName: {
-            validators: {
-                stringLength: {
-                    min: 5,
-                    message: 'Please Enter a task with minimum 5 letters length'
-                },
-                notEmpty: {
-                    message: 'Please Enter task name'
-                }
-            }
-        },
-       
-        taskDescription: {
-            validators: {
-                stringLength: {
-                    min: 15,
-                    max: 100,
-                    message:'Please enter at least 5 characters and no more than 100'
-                },
-                notEmpty: {
-                    message: 'Please Enter Task Description'
-                }
-            }
-        },
-        status: {
-            validators: {
-                notEmpty: {
-                    message: 'A status is required'
-                }
-            }
-        },
+//below creates hardcoding of new tasks (and pushes to this.tasks array)
+anotherTask.addTask('sleep', 'shut your eyes', 'saima', '11/02/2021', 'In Progress');
+anotherTask.addTask('study', 'open your laptop', 'fran', '22/02/2021', 'In Progress');
+anotherTask.addTask('eat', 'make sandwich', 'anya', '22/02/2021', 'In Progress');
+anotherTask.addTask('washing', 'clean football clothes', 'fran', '31/02/2021', 'In Progress');
 
-        date: {
-            validators: {
-                notEmpty: {
-                    message: 'A date is required'
-                }
-            }
-        },
-        
-        assignedTo: {
-            validators: {
-                notEmpty: {
-                    message: 'Choose a team member'
-                }
-            }
-        },
-        
-        }
-    });
+//need to get event listener working so that the form field inputs push 
+//to the array (instead of being hard coded). Code below is not working.
+
+const form = document.getElementById("task-form");
+form.addEventListener("submit", (sumbittedTask = () => {
+    let taskName = document.querySelector('#task-name');
+    let taskDescription = document.querySelector('#task-description');
+    let assignedTo = document.querySelector('#assigned-to');
+    let dueDate = document.querySelector('#due-date');
+    let status = document.querySelector('#status');
+    anotherTask.addTask(taskName.value , taskDescription.value , assignedTo.value , dueDate.value , status.value);
+    console.log(sumbittedTask);
+    
+}));
+
+
+/*validation - we used Bootrap instead 
+  const validFormFieldInput = (data) => {
+    const taskName = document.querySelector('#task-name');
+    const taskDescription = document.querySelector('#task-description');
+    const assignedTo = document.querySelector('#assigned-to');
+    const dueDate = document.querySelector('#due-date');
+    const status = document.querySelector('#status');
+    console.log("name:  " + taskDescription.value);
+  };
+
+validFormFieldInput(); */
+
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,1 +1,26 @@
+//create class
+class TaskManager {
+    constructor(currentId = 0) {
+        this.tasks = [];
+        this.currentId = currentId;
+    }
+
+addTask(taskName, description, assignedTo, dueDate, status) {
+//the method below creates multiple objects(instances)
+//but fist we need to save under a variable (called "task")
+    let task = {
+    id: this.currentId++,
+    taskName: taskName,
+    description: description,
+    assignedTo: assignedTo,
+    dueDate: dueDate,
+    status: status,
+    };
+    this.tasks.push(task);
+    
+}
+
+};
+
+
 
