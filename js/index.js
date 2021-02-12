3d343577395b8451c4fe841a99410d6f268cb88a
Anya-Sharky <75958034+Anya-Sharky@users.noreply.github.com>
2021-02-12 19:08:05 +1100

    test

--- js/index.js
+++ js/index.js
@@ -10,31 +10,28 @@ const form = document.getElementById("task-form");
 //anotherTask.addTask('eat', 'make sandwich', 'anya', '22/02/2021', 'In Progress');
 //anotherTask.addTask('washing', 'clean football clothes', 'fran', '31/02/2021', 'In Progress');
 
-
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let newTaskNameInput = document.querySelector('#task-name');
-    let newTaskDescription = document.querySelector('#task-description');
-    let newTaskAssignedTo = document.querySelector('#assigned-to');
-    let newTaskDueDate = document.querySelector('#due-date');
-    let newTaskStatus = document.querySelector('#status');
-   
+  event.preventDefault();
+  let newTaskNameInput = document.querySelector("#task-name");
+  let newTaskDescription = document.querySelector("#task-description");
+  let newTaskAssignedTo = document.querySelector("#assigned-to");
+  let newTaskDueDate = document.querySelector("#due-date");
+  let newTaskStatus = document.querySelector("#status");
 
-    let taskName =  newTaskNameInput.value;
-    let description = newTaskDescription.value;
-    let assignedTo = newTaskAssignedTo.value;
-    let dueDate = newTaskDueDate.value;
-    let status = newTaskStatus.value;
+  let taskName = newTaskNameInput.value;
+  let description = newTaskDescription.value;
+  let assignedTo = newTaskAssignedTo.value;
+  let dueDate = newTaskDueDate.value;
+  let status = newTaskStatus.value;
 
+  anotherTask.addTask(taskName, description, assignedTo, dueDate, status);
 
-anotherTask.addTask(taskName, description, assignedTo, dueDate, status);
+  newTaskNameInput.value = "";
+  newTaskDescription.value = "";
+  newTaskAssignedTo.value = "";
+  newTaskDueDate.value = "";
+  newTaskStatus.value = "";
 
-newTaskNameInput.value="";
-newTaskDescription.value="";
-newTaskAssignedTo.value="";
-newTaskDueDate.value="";
-newTaskStatus.value="";
-
-anotherTask.render();
-
-}); 
+  anotherTask.render();
+});
+hello;
