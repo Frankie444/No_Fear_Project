6f82bfc03f9852b351dfaad848f1d28a818f1f27
Frankie <75957770+Frankie444@users.noreply.github.com>
2021-02-12 14:06:23 +1100

    attempt dymanic push array

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const form = document.getElementById("task-form");
+
 //below creates an object instance using the TaskManager class
 const anotherTask = new TaskManager(0);
 console.log(anotherTask);
@@ -9,33 +11,26 @@ console.log(anotherTask);
 //anotherTask.addTask('eat', 'make sandwich', 'anya', '22/02/2021', 'In Progress');
 //anotherTask.addTask('washing', 'clean football clothes', 'fran', '31/02/2021', 'In Progress');
 
-const form = document.getElementById("task-form");
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     let newTaskNameInput = document.querySelector('#task-name');
     let newTaskDescription = document.querySelector('#task-description');
     let newTaskAssignedTo = document.querySelector('#assigned-to');
     let newTaskDueDate = document.querySelector('#due-date');
-    let newTaskstatus = document.querySelector('#status');
-    /*console.log("Task Name :" + taskName.value);
-    console.log("Task Description :" + taskDescription.value);
-    console.log("Task Assigned To :" + assignedTo.value);
-    console.log("Task Due Date :" + dueDate.value);
-    console.log("Task Status:" + status.value);*/
+    let newTaskStatus = document.querySelector('#status');
+   
 
-    // Get the values of the inputs
     let taskName =  newTaskNameInput.value;
     let description = newTaskDescription.value;
     let assignedTo = newTaskAssignedTo.value;
     let dueDate = newTaskDueDate.value;
-    let status = newTaskstatus.value;
+    let status = newTaskStatus.value;
 
-  
-    // Add the task to the task manager
+
 anotherTask.addTask(taskName, description, assignedTo, dueDate, status);
-
 
 }); 
 
 
-
+ 
